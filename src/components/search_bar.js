f09dc1a73e7f8bbdcd8dd6e156e92f84509ec03e
kimo5532 <shotbyabel@gmail.com>
2016-04-03 22:19:16 -0700

    React Classes & State

---
+++ src/components/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+//DEFINE COMPONENT
+//const SearchBar = () => { //function
+//return <input />//jsx
+//};
+
+//D E C L A R E  a  N E W  C L A S S 
+
+class SearchBar extends Component {
+  //constructor
+  constructor(props) {
+    super(props);//calling parent method
+
+    this.state = { term: ''};
+  }
+
+  render() {
+
+    return (
+      <div>
+      <input onChange={event => this.setState({ term: event.target.value })} />
+      Value of the input: {this.state.term}
+    </div>
+    );
+  }
+    // return <input onChange={this.onInputChange} />;
+    //   return <input onChange={event => console.log(event.target.value)} />;
+//event handler 
+// onInputChange(event) {
+//   console.log(event.target.value);
+//   // console.log(event);
+//   }
+}
+
+export default SearchBar;
